6ccd07c9f50fab2673aa03a73ed0a2dc924b6e03
binna <every5116@naver.com>
2020-11-08 13:42:46 +0900

    로그인

--- src/main/java/com/server/login/controller/LoginController.java
+++ src/main/java/com/server/login/controller/LoginController.java
@@ -1,39 +1,30 @@
 package com.server.login.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
+@RequestMapping("/login/*")
 public class LoginController {
+
+	@GetMapping("/loginMain")
+	public void loginInput(String error, String logout, Model model) {
+		System.out.println("error: " + error);
+		System.out.println("logout: " + logout);
+		
+		if(error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("logout", "Logout!!");
+		}
+	} 
 	
-	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@GetMapping("loginAfter")
+	public void loginAfter() {
+		System.out.println("로그인 성공");
 	}
-	
 }
