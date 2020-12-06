8a940aa23bcb3b423904c72387dec2e672eb71db
binna <every5116@naver.com>
2020-12-07 00:02:33 +0900

    로그인

--- src/main/webapp/ERD/binna/DML.sql
+++ src/main/webapp/ERD/binna/DML.sql
@@ -1,7 +1,10 @@
 SELECT * FROM shineTetris.tcm_member;
 
-ALTER TABLE shineTetris.tcm_member CHANGE user_pw USER_PW varchar(1000);
+ALTER TABLE shineTetris.tcm_member CHANGE password user_pw varchar(1000);
+ALTER TABLE shineTetris.tcm_member CHANGE username user_id varchar(45);
+ALTER TABLE shineTetris.tcm_member CHANGE USER_NM user_name varchar(20);
+ALTER TABLE shineTetris.tcm_member ADD authority varchar(100);
 
 SELECT USER_ID username, USER_PW password FROM shineTetris.tcm_member;
 
-UPDATE shineTetris.tcm_member set enabled = '1';
+UPDATE shineTetris.tcm_member set authority = 'ROLE_USER';
